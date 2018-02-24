fe3c4adccfe757eb26cabf1ae36689481b66e2be
beniaminp <carol1234>
2018-02-24 10:40:55 +0200

    wip

--- src/main/scala/com/padana/fb/main/FBApi.scala
+++ src/main/scala/com/padana/fb/main/FBApi.scala
@@ -26,7 +26,7 @@ class FBApi(userfbId: String, accessToken: String) {
 }
 
 object FBApi extends App {
-  var fBApi: FBApi = new FBApi("x", "xx")
+  var fBApi: FBApi = new FBApi("1706661999352250", "EAACdEDMoDMQBALGniXl6t6MckjlzoLTY4TBbl9Tw4zsSbgSsgtmz5932ZBW2SMfUrbrLLo37Axk4cZAIeIfXClp1mdtHI3QUHdK5W0ewooP2nQbNd1rddTbdqIslzZBYCoyOF53AGRmeDtBtbjiFCD6fmTuD5ILCrQ76r3ZBFecSyLAuC30fN1HxOZCk1BGQCZAPHzxUyOJFSWPlPgJD9q")
   fBApi.getAlbumApi().getAlbums().foreach(a => {
     println("Album: " + a)
   })
